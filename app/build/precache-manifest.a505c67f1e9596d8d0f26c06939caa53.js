f4fa167e43de7303d2829d1e612fae1bc4d61e94
amyre <benhammouda.amir@gmail.com>
2020-02-07 14:42:16 +0100

    build

---
+++ app/build/precache-manifest.a505c67f1e9596d8d0f26c06939caa53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646b5331b8e2cd94fd5c07e8ed647bdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b11c1a762825b944ac4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f45cbef3b0f091175fdb",
+    "url": "/static/css/main.88387311.chunk.css"
+  },
+  {
+    "revision": "3b11c1a762825b944ac4",
+    "url": "/static/js/2.86e47155.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.86e47155.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f45cbef3b0f091175fdb",
+    "url": "/static/js/main.959b2272.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  }
+]);
